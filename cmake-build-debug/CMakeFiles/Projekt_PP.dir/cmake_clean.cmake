22b9a63dabac407035a9365268f369627ef914e4
Lukasz Halicki <uhalicki@gmail.com>
2023-01-13 18:44:44 +0100

    fix

---
+++ cmake-build-debug/CMakeFiles/Projekt_PP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt_PP.dir/main.c.obj"
+  "CMakeFiles/Projekt_PP.dir/main.c.obj.d"
+  "Projekt_PP.exe"
+  "Projekt_PP.exe.manifest"
+  "Projekt_PP.pdb"
+  "libProjekt_PP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projekt_PP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Projekt_PP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Projekt_PP.
+# This may be replaced when dependencies are built.
